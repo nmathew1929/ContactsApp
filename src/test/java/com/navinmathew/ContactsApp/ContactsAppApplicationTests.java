ba439a81ab949d216247c016864cfe596270bf7e
Navin Mathew <navinmathew3@gmail.com>
2020-02-13 14:43:26 -0600

    first commit

---
+++ src/test/java/com/navinmathew/ContactsApp/ContactsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navinmathew.ContactsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
